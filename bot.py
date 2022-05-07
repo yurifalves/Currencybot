04a199a6f7522367c43c763722d007cfbc267297
Yuri <87913263+yurifalves@users.noreply.github.com>
2022-05-06 23:43:32 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+class Bot:
 
+    @staticmethod
+    def token():
+        telegramtoken = TOKEN  # TOKEN do bot
+        return telegramtoken
+
+    @staticmethod
+    def executar():
+        import config
+
+
+Bot.executar()
